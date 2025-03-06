1b0cabac8cbf7b09bc79917c99aeb8dea1c8ae60
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2025-03-06 17:20:05 -0500

    Env variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: true,
+  /**
+   * * Firebase configuration
+   * * Replace with your own firebase project configuration
+   * ! Do not share your configuration
+   * * Go to your firebase project settings and get your configuration object
+   */
+  firebase: {
+    apiKey: '<API_KEY>',
+    authDomain: '<AUTH_DOMAIN>',
+    projectId: '<PROJECT_ID>',
+    storageBucket: '<STORAGE_BUCKET>',
+    messagingSenderId: '<MESSAGING_SENDER_ID>',
+    appId: '<APP_ID>',
+    measurementId: '<MEASUREMENT_ID>',
+  }
+};
